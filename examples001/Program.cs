2ac59500535817d54bfca329148480ee2e76934f
Farhod <temirovfarhod.26@gmail.com>
2022-12-03 18:44:17 +0600

    программы с урока

--- examples001/Program.cs
+++ examples001/Program.cs
@@ -1,2 +1,33 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// int number = new Random().Next(10, 100); // Next (start, finish + 1)
+
+// int firstDigit = number / 10;//Первая цифра числа: 78 -> 7
+
+// int secondDigit = number % 10; //Вторая цифра числа: 78 -> 8
+
+// int max = firstDigit; // Пусть максимум - первая цифра числа
+
+// if (secondDigit > max ) // max = 5, secondDigit  = 10
+// {
+//     max = secondDigit;
+// }
+// Console.WriteLine("Исходное число: " + number);
+// Console.WriteLine("Максимальное число: " + max);
+
+// Первое задание
+// int number = new Random().Next(100, 1000);
+// int firstNumber = number/100;
+// int secondNumber = number%10;
+// System.Console.WriteLine(number);
+// System.Console.WriteLine(firstNumber);
+// System.Console.WriteLine(secondNumber);
+// System.Console.WriteLine(firstNumber * 10 + secondNumber);
+
+
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % 7 == 0 && number % 23 == 0)
+// {
+//     System.Console.WriteLine("кратно");
+// }
+// else System.Console.WriteLine("не кратно");
